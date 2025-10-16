84f1ed7895b9b51ec35729dd2afdff1be16aaf67
Muhamad Jalaludin Rumi <muhammadrumi876@gmail.com>
2025-10-16 15:44:42 +0700

    auth controlller lagi

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,8 +18,11 @@ use App\Http\Controllers\UserAuthorityController;
 use App\Http\Controllers\AclController;
 
 //Route Auth
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware('web')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
 Route::middleware('auth:sanctum')->group(function () {
     // route buat cek user & auth status
     Route::get('/me', function (Request $request) {
