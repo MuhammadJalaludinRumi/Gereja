65297d3371c3e753611a5e54dfff4ef65cf60383
Muhamad Jalaludin Rumi <muhammadrumi876@gmail.com>
2025-10-16 15:51:04 +0700

    sekarang api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\AclController;
 Route::middleware('web')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
 });
 
 Route::middleware('auth:sanctum')->group(function () {
