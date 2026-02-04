918d176728a090a51a3fcfa95a9f81199de2f66d
[Bennart <[bennart.dgunawan@gmail.com]>
2026-02-04 10:10:08 +0700

    Fix Refactor Announcement and Reflection controllers to filter by organization ID and update API routes

--- backend/app/Http/Controllers/AnnouncementController.php
+++ backend/app/Http/Controllers/AnnouncementController.php
@@ -4,17 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Announcement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AnnouncementController extends Controller
 {
     public function index()
     {
-        return response()->json(Announcement::orderBy('date_post', 'desc')->get());
+        return response()->json(Announcement::where('organization_id', Auth::user()->role->organization_id)->orderBy('date_post', 'desc')->get());
     }
 
     public function latest()
     {
-        return response()->json(Announcement::latest('date_post')->first());
+        return response()->json(Announcement::where('organization_id', Auth::user()->role->organization_id)->latest('date_post')->first());
     }
 
     public function show($id)
@@ -30,6 +31,8 @@ class AnnouncementController extends Controller
             'content' => 'required|string',
             'status' => 'required|boolean'
         ]);
+
+        $data['organization_id'] = Auth::user()->role->organization_id; 
 
         $announcement = Announcement::create($data);
         return response()->json($announcement, 201);
